3bb60aab86701e7f781d878af88466ed859d28ed
Yash Rathore <yash@reportgarden.com>
2025-10-21 14:46:59 +0530

    sdasd

--- .github/scripts/auto-merge.js
+++ .github/scripts/auto-merge.js
@@ -71,7 +71,7 @@ async function waitForChecks(prSha) {
   const start = Date.now();
   while ((Date.now() - start) / 1000 < POLL_TIMEOUT) {
     const { data } = await octokit.checks.listForRef({ owner, repo, ref: prSha });
-    const checks = data.check_runs.filter(c => !c.name.includes("Auto Merge Bot"));
+    const checks = data.check_runs.filter(c => !c.name.includes("auto-merge"));
 
     if (checks.length === 0) return true;
 
